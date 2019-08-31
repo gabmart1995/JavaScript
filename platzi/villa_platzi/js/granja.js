062c7761f1dac01ba7b68ddfe6cd9fe62527b7f5
Gabriel Martínez <gabmart1995@gmail.com>
2019-08-31 15:53:03 -0400

    Finalizacion del ejercicio de la Villa

--- platzi/villa_platzi/js/granja.js
+++ platzi/villa_platzi/js/granja.js
@@ -26,13 +26,7 @@ var granja = {
 		ok: false,
 
 		dibujarCerdo: function( x, y ) {
-			
 			papel.drawImage( cerdo, x, y );
-		},
-
-		moverCerdo: function( event ) {
-
-			
 		}
 	},
 
@@ -44,10 +38,54 @@ var granja = {
 		dibujarPollo: function( x, y ) {
 			papel.drawImage( pollo, x, y );
 		}
+	},
+
+	recargarCanvas: function( x, y ) {
+
+		/*
+			Se recarga todo el canvas desde el fondo
+			y todos los animales
+		*/
+
+		var posXVaca = 0; 
+		var posYVaca = 0;
+		var posXPollo = 0;
+		var posYPollo = 0;
+
+		granja.mapa.dibujarMapa(); 
+
+		for ( var p = 0; p < cantidadPollos; p++ ) {
+
+			if ( ( p % 2 ) === 0 ) {
+				posXPollo = coordPollos[p];
+			}
+
+			else {
+				posYPollo = coordPollos[p];
+			}
+
+			granja.pollo.dibujarPollo( posXPollo, posYPollo );
+		}
+
+		for ( var v = 0; v < cantidadVacas; v++ ) {
+
+			if ( ( v % 2 ) === 0 ) {
+				posXVaca = coordVacas[v];
+			}
+
+			else {
+				posYVaca = coordVacas[v];
+			}
+
+			granja.vaca.dibujarVaca( posXVaca, posYVaca );
+		}
+
+		granja.cerdo.dibujarCerdo( x, y );
 	}
 };
 
 var teclas = {
+	
 	UP: 38,
 	DOWN: 40,
 	LEFT: 37,
@@ -55,11 +93,16 @@ var teclas = {
 };
 
 // cantidad de animales
-var cantidad = 0;
+var cantidadVacas = 0;
+var cantidadPollos = 0;
 var movimiento = 5;
 
+// coordenadas
 var x, y;
+var coordPollos = [];
+var coordVacas = [];
 
+// elementos HTML
 var vp = document.getElementById( 'villaPlatzi' );
 var papel = vp.getContext( '2d' );
 
@@ -73,8 +116,9 @@ vaca.src = granja.vaca.url;
 pollo.src = granja.pollo.url;
 cerdo.src = granja.cerdo.url;
 
+// eventos
 fondo.addEventListener( 'load', validarCarga );
-document.addEventListener( 'keydown', moverPersonaje );
+document.addEventListener( 'keydown', moverCerdo );
 
 function validarCarga() {
 
@@ -95,9 +139,10 @@ function dibujar() {
 
 	if ( granja.vaca.ok ) {
 
-		cantidad = obtenerNumeroAleatorio( 5, 15 );
+		cantidadVacas = obtenerNumeroAleatorio( 5, 15 );
+		// console.log( cantidadVacas );
 
-		for ( var v = 0; v < cantidad; v++ ) {
+		for ( var v = 0; v < cantidadVacas; v++ ) {
 
 			var coorX = obtenerNumeroAleatorio( 0, 10 );
 			var coorY = obtenerNumeroAleatorio( 0, 10 );
@@ -106,14 +151,18 @@ function dibujar() {
 			coorY *= 40;
 
 			granja.vaca.dibujarVaca( coorX, coorY );
+
+			// guarda las coordenadas en el arreglo
+			coordVacas.unshift( coorX, coorY );
 		}
 	}
 
 	if ( granja.pollo.ok ) {
 
-		cantidad = obtenerNumeroAleatorio( 5, 15 );
+		cantidadPollos = obtenerNumeroAleatorio( 5, 15 );
+		// console.log( cantidadPollos );
 
-		for ( var p = 0; p < cantidad; p++ ) {
+		for ( var p = 0; p < cantidadPollos; p++ ) {
 
 			var coorX = obtenerNumeroAleatorio( 0, 10 );
 			var coorY = obtenerNumeroAleatorio( 0, 10 );
@@ -122,6 +171,9 @@ function dibujar() {
 			coorY *= 40;
 
 			granja.pollo.dibujarPollo( coorX, coorY );
+
+			// coloca las coordenadas en el arreglo
+			coordPollos.unshift( coorX, coorY );
 		}
 	}
 
@@ -133,6 +185,7 @@ function dibujar() {
 		coorX *= 40;
 		coorY *= 40;
 
+		// movimiento
 		x = coorX;
 		y = coorY;
 
@@ -147,41 +200,41 @@ function obtenerNumeroAleatorio( min, max ) {
 	return resultado;
 }
 
-function moverPersonaje( event ) {
+function moverCerdo( event ) {
 
 	switch ( event.keyCode ) {
 
 		case teclas.UP:
-					
-			granja.cerdo.dibujarCerdo( x, y - movimiento );
+			
+			granja.recargarCanvas( x, y - movimiento );
 			y -= movimiento;
 
 		break;
 
 		case teclas.DOWN:
 					
-			granja.cerdo.dibujarCerdo( x, y + movimiento );
+			granja.recargarCanvas( x, y + movimiento );
 			y += movimiento;
 
 		break;
 
 		case teclas.LEFT:
 					
-			granja.cerdo.dibujarCerdo( x - movimiento, y );
+			granja.recargarCanvas( x - movimiento, y );
 			x -= movimiento;
 
 		break;
 
 		case teclas.RIGHT:
 					
-			granja.cerdo.dibujarCerdo( x + movimiento, y );
+			granja.recargarCanvas( x + movimiento, y );
 			x += movimiento;
 
 		break;
 
 		default:
 
-			console.log( 'La tecla que selecciono es incorrecto' );
+			// console.log( 'La tecla que selecciono es incorrecto' );
 
 		break;
 	}
